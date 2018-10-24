cf46f959c6369bd74f608360244bd668a5e35ec8
Gorlami96 <30335782+Gorlami96@users.noreply.github.com>
2018-10-24 10:39:32 +0530

    Update PageRank.py
    No functional changes . Made code more readable

--- PageRank/PageRank.py
+++ PageRank/PageRank.py
@@ -9,13 +9,14 @@ def create_graph(filename):
 ##    This function accepts a filename string as a parameter.
 ##    This filename contains edgelist in the format of "node1 node2"
 ##    which indicates the former node points to the latter. Using
-##    read_esgelist function of the networkx library , we can easily
-##    make a drected graph from the given information. The function
+##    read_edgelist function of the networkx library , we can easily
+##    make a directed graph from the given information. The function
 ##    returns a directed graph.
     
     g=nx.read_edgelist(filename,create_using=nx.DiGraph())
     n=nx.number_of_nodes(g)
     g=nx.stochastic_graph(g)
+    
     return g,n
 
 def create_adjacency_matrix(G,N):
@@ -28,16 +29,20 @@ def create_adjacency_matrix(G,N):
     
     a=np.zeros(N*N).reshape(N,N)
     no_ones_per_row=dict.fromkeys(range(N),0)
+    
     for i in G.edges():
         r=int(i[0])-1
         c=int(i[1])-1
         a[r,c]=1
         no_ones_per_row[r]+=1
+        
     for i in range(N):
         if no_ones_per_row[i]!=0 :
             for j in range(N):
                 a[i,j]=float(a[i,j])/float(no_ones_per_row[i])
+                
     dead_ends=[]
+    
     for i in no_ones_per_row :
         if no_ones_per_row[i]==0 :
             dead_ends.append(i)
@@ -54,11 +59,14 @@ def fix_dead_ends(A,N,dead_ends):
 ##    deistributed.
     
     d=np.zeros(N).reshape(N,1) 
+    
     for i in dead_ends :
         d[i]=1.0
+        
     w=np.ones(N)/N  ####
     w=w.reshape(1,6012)
     s=A+np.dot(d,w)
+    
     return s
 
 def create_google_matrix(S,N,beta,p):
@@ -69,6 +77,7 @@ def create_google_matrix(S,N,beta,p):
     ones=np.ones(N).reshape(N,1)
     temp=np.dot(ones,p)
     GM=beta*S + (1-beta)*temp
+    
     return GM
 
 def return_answer(GM,sv,number_of_iter):
@@ -78,9 +87,11 @@ def return_answer(GM,sv,number_of_iter):
     
     data_history=[]
     data_history.append(sv)
+    
     for i in range(number_of_iter):
         sv=np.dot(sv,GM)
         data_history.append(sv)
+        
     return sv,data_history
 
 def plot_graph(G,ans):
@@ -88,12 +99,16 @@ def plot_graph(G,ans):
 ##    constructs the required graph by selecting some randomly connected nodes.
     
     nl=[]
+    
     for i in range(20):
         nl.append(str(random.randint(1,100)))
+        
     H=G.subgraph(nl)
     ns=[]
+    
     for i in H.nodes():
         ns.append(answer[0][int(i)-1]*100000)
+        
     nx.draw_networkx(H,pos=nx.spring_layout(H),node_size=ns)
     plt.show()
 
@@ -101,8 +116,10 @@ def plot_graph_max(data_histry,max_ind):
     
     x=[i for i in range(1,11)]
     y=[]
+    
     for i in range(11):
         y.append(data_history[i][0][max_ind])
+        
     plt.plot(x,y)
     plt.show()
 
